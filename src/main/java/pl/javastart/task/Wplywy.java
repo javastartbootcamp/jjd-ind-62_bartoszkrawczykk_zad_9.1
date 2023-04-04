f7ba0e005aad4fb9a81b58db1cc39942e942bdbf
Bartek Krawczyk <bartoszkrawczykk@gmail.com>
2023-04-04 14:53:27 +0200

    rozwiązanie 1

---
+++ src/main/java/pl/javastart/task/Wplywy.java
@@ -0,0 +1,17 @@
+package pl.javastart.task;
+
+public class Wplywy {
+    private double wplywy;
+
+    public Wplywy(double wplywy) {
+        this.wplywy = wplywy;
+    }
+
+    public double getWplywy() {
+        return wplywy;
+    }
+
+    public void setWplywy(double wplywy) {
+        this.wplywy = wplywy;
+    }
+}
---
+++ src/main/java/pl/javastart/task/Wydatki.java
@@ -0,0 +1,17 @@
+package pl.javastart.task;
+
+public class Wydatki {
+    private double wydatki;
+
+    public Wydatki(double wydatki) {
+        this.wydatki = wydatki;
+    }
+
+    public double getWydatki() {
+        return wydatki;
+    }
+
+    public void setWydatki(double wydatki) {
+        this.wydatki = wydatki;
+    }
+}
